fe504d38a32e596fb1ff9a8e8fb03a1f62bf234e
Valeria Klyuchnikova <v.klyuchnikova@kingbird.ru>
2024-04-12 19:48:01 +0800

    Deploy

--- src/router.tsx
+++ src/router.tsx
@@ -5,34 +5,37 @@ import ContactsPage from "./features/contacts";
 import ContactPage from "./features/contacts/modules/contact";
 import ContactForm from "./features/contacts/modules/form";
 
-const router = createBrowserRouter([
-  {
-    element: <IndexPage />,
-    children: [
-      {
-        path: "auth",
-        element: <AuthPage />,
-      },
-      {
-        path: "contacts",
-        element: <ContactsPage />,
-        children: [
-          {
-            path: ":id",
-            element: <ContactPage />,
-          },
-          {
-            path: ":id/edit",
-            element: <ContactForm action='edit' />,
-          },
-          {
-            path: "new",
-            element: <ContactForm action='create' />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <IndexPage />,
+      children: [
+        {
+          path: "auth",
+          element: <AuthPage />,
+        },
+        {
+          path: "contacts",
+          element: <ContactsPage />,
+          children: [
+            {
+              path: ":id",
+              element: <ContactPage />,
+            },
+            {
+              path: ":id/edit",
+              element: <ContactForm action='edit' />,
+            },
+            {
+              path: "new",
+              element: <ContactForm action='create' />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: "/demo-contacts/" }
+);
 
 export default router;
